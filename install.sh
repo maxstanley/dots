380a1353564e8a36b26215059c22d65e18611940
Max Stanley <max@maxstanley.uk>
2020-12-21 11:58:06 +0000

    Added install.sh file

---
+++ install.sh
@@ -0,0 +1,48 @@
+pacman -S \
+	tmux \
+	dhcpcd \
+	sudo \
+	neovim \
+	iwd \
+	openssh \
+	nftables \
+	man-db \
+	man-pages \
+	wget \
+	curl \
+	git \
+	base-devel \
+	go \
+	go-tools \
+	i3-wm \
+	i3status \
+	xorg-server \
+	xorg-xinit \
+	firefox \
+	ttf-roboto-mono
+	python3 \
+	python-pip \
+	nodejs-lts-fermium \
+	yarn \
+	xfce4-power-manager \
+	xorg-xinput \
+	clang \
+	openvpn \
+	rustup \
+	powertop \
+	lightdm-gtk-greeter \
+	htop
+
+# yay
+
+yay -S \
+	docker \
+	alacritty \
+	dmenu2 \
+	ttf-ms-fonts \
+	visual-studio-code-bin \
+	tlp \
+	tlpui-git \
+	linux-lts \
+	linux-lts-headers \
+	mpv
