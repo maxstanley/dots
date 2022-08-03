3351c21e826424d66a5ecf94487466ae4b2d7aea
Max Stanley <github@maxstanley.uk>
2022-08-03 21:14:27 +0000

    neovim add neogit key binding.

--- nvim/lua/maxstanley/ide/git.lua
+++ nvim/lua/maxstanley/ide/git.lua
@@ -1,6 +1,10 @@
 local gitsigns = require("gitsigns")
 local neogit = require("neogit")
 
+local nnoremap = require("maxstanley.keymap").nnoremap
+
 gitsigns.setup({})
 neogit.setup({})
+
+nnoremap("<Leader>g", function() neogit.open() end)
 
