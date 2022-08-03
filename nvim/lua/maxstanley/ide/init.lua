5d7e08c1ae83cbf05f11231148338e177639e3c7
Max Stanley <github@maxstanley.uk>
2022-08-03 21:38:36 +0000

    neovim undotree for better undo support.

--- nvim/lua/maxstanley/ide/init.lua
+++ nvim/lua/maxstanley/ide/init.lua
@@ -1,7 +1,11 @@
 local autopairs = require("nvim-autopairs")
 
+local nnoremap = require("maxstanley.keymap").nnoremap
+
 autopairs.setup({})
 
 vim.ui.select = require("popui.ui-overrider")
 vim.ui.input = require("popui.input-overrider")
+
+nnoremap("<Leader>u", ":UndotreeShow<CR>")
 
